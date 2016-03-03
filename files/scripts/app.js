87c7729561e5d4a2dd4c8cd00fb1af48d255d040
Tan He <tan.he@uwaterloo.ca>
2016-03-02 23:16:19 -0500

    Improved code readability and structure

--- files/scripts/app.js
+++ files/scripts/app.js
@@ -1,42 +1,43 @@
-var main = function(){
-    
-    $('.nav-icon').click(function(){
-        $('html, body').stop().animate({scrollTop: '0px'},900);
+var main = function () {
+
+    $('.nav-icon').click(function () {
+        $('html, body').stop().animate({
+            scrollTop: '0px'
+        }, 900);
     });
-    
+
     $('.dropdown-button').click(slide);
     $('.close-bar span').click(slide);
 }
 
-var slide = function(){
+var slide = function () {
     $('.dropdown-button').toggleClass('active')
     $('#about').slideToggle('700');
 }
 
 
-$(document).ready(function(){
-	$('a[href^="#"]').on('click',function (e) {
-	    e.preventDefault();
+$(document).ready(function () {
+    $('a[href^="#"]').on('click', function (e) {
+        e.preventDefault();
 
-	    var target = this.hash;
-	    var $target = $(target);
+        var target = this.hash;
+        var $target = $(target);
 
-        if(target=='#about' && !$('.dropdown-button').hasClass('active'))
+        if (target == '#about' && !$('.dropdown-button').hasClass('active'))
             $('.dropdown-button').click();
-        
-	    $('html, body').stop().animate({
-	        scrollTop: $target.offset().top
-	    }, 900, 'swing', function () {
-	        window.location.hash = target;
-	    });
-	});
-    
-    $(window).scroll(function(){
+
+        $('html, body').stop().animate({
+            scrollTop: $target.offset().top
+        }, 900, 'swing', function () {
+            window.location.hash = target;
+        });
+    });
+
+    $(window).scroll(function () {
         console.log($(window).scrollTop());
-        if($(window).scrollTop() > 452){
+        if ($(window).scrollTop() > 452) {
             $('.navbar-inverse').addClass('solid');
-        }
-        else if($(window).scrollTop() <= 452){
+        } else if ($(window).scrollTop() <= 452) {
             $('.navbar-inverse.solid').removeClass('solid');
         }
     });
