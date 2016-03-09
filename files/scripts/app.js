3a57b553bd68c74e715db81492652cbfb76afa34
Tan He <tan.he@uwaterloo.ca>
2016-03-08 19:17:13 -0500

    Refactored app.js

--- files/scripts/app.js
+++ files/scripts/app.js
@@ -1,4 +1,33 @@
-var main = function () {
+$(document).ready(function () {
+
+    $('a[href^="#"]').on('click', function (e) {
+        e.preventDefault();
+
+        var target = this.hash,
+            $target = $(target);
+
+        if (target == '#about' && !$('.dropdown-button').hasClass('active')) {
+            $('.dropdown-button').click();
+        }
+        $('html, body').stop().animate({
+            scrollTop: $target.offset().top
+        }, 900, 'swing', function () {
+            window.location.hash = target;
+        });
+    });
+
+    var slide = function () {
+        $('.dropdown-button').toggleClass('active');
+        $('#about').slideToggle('700');
+    };
+
+    $(window).scroll(function () {
+        if ($(window).scrollTop() > 506) {
+            $('.navbar-inverse').addClass('solid');
+        } else if ($(window).scrollTop() <= 506) {
+            $('.navbar-inverse.solid').removeClass('solid');
+        }
+    });
 
     $('.nav-icon').click(function () {
         $('html, body').stop().animate({
@@ -8,39 +37,5 @@ var main = function () {
 
     $('.dropdown-button').click(slide);
     $('.close-bar span').click(slide);
-}
 
-var slide = function () {
-    $('.dropdown-button').toggleClass('active')
-    $('#about').slideToggle('700');
-}
-
-
-$(document).ready(function () {
-    $('a[href^="#"]').on('click', function (e) {
-        e.preventDefault();
-
-        var target = this.hash;
-        var $target = $(target);
-
-        if (target == '#about' && !$('.dropdown-button').hasClass('active'))
-            $('.dropdown-button').click();
-
-        $('html, body').stop().animate({
-            scrollTop: $target.offset().top
-        }, 900, 'swing', function () {
-            window.location.hash = target;
-        });
-    });
-
-    $(window).scroll(function () {
-        console.log($(window).scrollTop());
-        if ($(window).scrollTop() > 452) {
-            $('.navbar-inverse').addClass('solid');
-        } else if ($(window).scrollTop() <= 452) {
-            $('.navbar-inverse.solid').removeClass('solid');
-        }
-    });
 });
-
-$(document).ready(main);
