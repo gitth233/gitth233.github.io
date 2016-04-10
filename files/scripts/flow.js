fffce439e0c2df29fb03d66ac8199327fe048b95
Tan He <tan.he@uwaterloo.ca>
2016-04-10 17:44:12 -0400

    Commented javascript code and fixed small errors

--- files/scripts/flow.js
+++ files/scripts/flow.js
@@ -11,6 +11,7 @@ var FlowDrawer = function () {
         lineWidth = 3,
         toDegree = Math.PI / 180;
 
+    // Circle Object, moveY is the object's moving speed on the y-axis
     function Circle(x, y, radius, moveY) {
         this.x = x;
         this.y = y;
@@ -18,6 +19,7 @@ var FlowDrawer = function () {
         this.moveY = moveY;
     }
 
+    // Circle Object's Draw Method
     Circle.prototype.draw = function () {
         ctx.beginPath();
         ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
@@ -25,6 +27,7 @@ var FlowDrawer = function () {
         ctx.stroke();
     };
 
+    // Square Object, moveY is the object's moving speed on the y-axis, angle is the initial rotation the object has
     function Square(x, y, length, moveY, angle, angularSpeed) {
         this.x = x;
         this.y = y;
@@ -34,6 +37,7 @@ var FlowDrawer = function () {
         this.angularSpeed = angularSpeed;
     }
 
+    // Square Object's Draw Method
     Square.prototype.draw = function () {
         ctx.translate(this.x, this.y);
         ctx.rotate(this.angle * toDegree);
@@ -42,6 +46,7 @@ var FlowDrawer = function () {
         ctx.setTransform(1, 0, 0, 1, 0, 0);
     };
 
+    // Triangle Object, moveY is the object's moving speed on the y-axis, angle is the initial rotation the object has
     function Triangle(x, y, length, moveY, angle, angularSpeed) {
         this.x = x;
         this.y = y;
@@ -50,10 +55,12 @@ var FlowDrawer = function () {
         this.angle = angle;
         this.angularSpeed = angularSpeed;
 
+        // The distance from the centre of the object to the origin of the canvas
         this.xFromOrigin = this.length / 2;
         this.yFromOrigin = Math.sqrt(3) / 4 * this.length;
     }
 
+    // Triangle Object's Draw Method
     Triangle.prototype.draw = function () {
         ctx.translate(this.x, this.y);
         ctx.rotate(this.angle * toDegree);
@@ -77,6 +84,7 @@ var FlowDrawer = function () {
         HEIGHT = height;
     }
 
+    // Call the function when the size of the canvas is changed
     function resize(width, height) {
         var check;
         setCanvasSize(width, height);
@@ -85,28 +93,32 @@ var FlowDrawer = function () {
         ctx.strokeStyle = strokeStyle;
         ctx.lineWidth = lineWidth;
         ctx.lineJoin = 'round';
+
+        // Check if the canvas requires to change the number of the objects
         check = changeObjNumOnResize();
         if (objNum != check) {
             objNum = check;
         }
     }
 
+
+    // If the width of the current canvas is larger than 1366px, use the first element in the resolution array, if it's between 1366px and 640px, use the second element, if it's less than 640px, use the third element
     function changeObjNumOnResize() {
-        if (WIDTH > 1366){
+        if (WIDTH > 1366) {
             return 0;
-        }
-        else if (WIDTH > 640){
+        } else if (WIDTH > 640) {
             return 1;
-        }
-        else{
+        } else {
             return 2;
         }
     }
 
+    // Return the number of objects drawn on the canvas
     function getObjNum() {
         return resolution[objNum];
     }
 
+    // Set the number of objects to be drawn on the canvas. Three parameters for three differernt screen resolution; larger than 1366, between 1366px and 640px, and less than 640px 
     function setObjNum(num1, num2, num3) {
         resolution[0] = num1;
         resolution[1] = num2;
@@ -129,15 +141,18 @@ var FlowDrawer = function () {
         lineWidth = width;
     }
 
+    // Return an integer random number; min value is optional (default is set to 0) 
     function randomNum(max, min) {
         min = arguments[1] || 0;
         return Math.floor(Math.random() * (max - min + 1) + min);
     }
 
+    // Return either 0 or 1; used to indicate the direction of rotation
     function getDirection() {
         return randomNum(2) === 0 ? -1 : 1;
     }
 
+    // Initailze the canvas
     function init() {
         canvas = document.getElementById(canvasID);
         ctx = canvas.getContext('2d');
@@ -145,8 +160,9 @@ var FlowDrawer = function () {
         canvas.height = HEIGHT;
         ctx.strokeStyle = strokeStyle;
         ctx.lineWidth = lineWidth;
-        ctx.lineJoin ="round";
-        
+        ctx.lineJoin = "round";
+
+        // Create the objects based on the current size of the canvas
         objNum = changeObjNumOnResize(WIDTH);
         for (var x = 0; x < resolution[objNum]; x++) {
             createNewObj();
@@ -154,15 +170,17 @@ var FlowDrawer = function () {
         window.requestAnimationFrame(draw);
     }
 
+    // Create Objects function
     function createNewObj() {
-        objList.push(new Square(randomNum(WIDTH), randomNum(HEIGHT), 20, randomNum(1, 4), randomNum(0, 90), randomNum(1, 3) * getDirection()));
+        objList.push(new Square(randomNum(WIDTH), randomNum(HEIGHT), 20, randomNum(1, 2), randomNum(0, 90), randomNum(1, 3) * getDirection()));
 
-        objList.push(new Circle(randomNum(WIDTH), randomNum(HEIGHT), 10, randomNum(1, 4)));
+        objList.push(new Circle(randomNum(WIDTH), randomNum(HEIGHT), 10, randomNum(1, 2)));
 
-        objList.push(new Triangle(randomNum(WIDTH), randomNum(HEIGHT), 20, randomNum(1, 4), randomNum(0, 90), randomNum(1, 3) * getDirection()));
+        objList.push(new Triangle(randomNum(WIDTH), randomNum(HEIGHT), 20, randomNum(1, 2), randomNum(0, 90), randomNum(1, 3) * getDirection()));
     }
 
-    function draw(timestamp) {
+    // Update the graph with objects' current status
+    function draw() {
         ctx.clearRect(0, 0, canvas.width, canvas.height);
         for (var x = 0; x < resolution[objNum] * 3; x++) {
             var obj = objList[x];
@@ -185,6 +203,7 @@ var FlowDrawer = function () {
         window.requestAnimationFrame(draw);
     }
 
+    // Expose only the public functions to the user
     return {
         setCanvasID: setCanvasID,
         setCanvasSize: setCanvasSize,
--- files/scripts/wave.js
+++ files/scripts/wave.js
@@ -7,7 +7,7 @@ var WaveDrawer = function () {
         ctx,
         wave;
 
-
+    // Initialize the canvas 
     function init() {
         canvas = document.getElementById(canvasID);
         ctx = canvas.getContext('2d');
@@ -17,8 +17,11 @@ var WaveDrawer = function () {
         ctx.lineJoin = 'round';
 
         wave = new sineWave();
-        if (WIDTH <= 640)
-            wave.VALUE = Math.PI / 100;
+
+        // If the screen size is less than 640 px, decrease the frequency of the sine function
+        if (WIDTH <= 640) {
+            wave.FREQUENCY = Math.PI / WIDTH;
+        }
 
         window.requestAnimationFrame(draw);
     }
@@ -39,40 +42,46 @@ var WaveDrawer = function () {
     function setAmplitude(amplitude) {
         wave.amplitude = amplitude;
     }
+    /*
+        function random(max, min) {
+            min = arguments[1] || 0;
+            return Math.floor(Math.random() * (max - min + 1)) + min;
+        }
+    */
 
-    function random(max, min) {
-        min = arguments[1] || 0;
-        return Math.floor(Math.random() * (max - min + 1)) + min;
-    }
-
+    // Sine wave object 
     function sineWave() {
-        this.lineWidth = WIDTH / 100;
         this.amplitude = 30;
         this.counter = 1;
-        this.VALUE = 2 * Math.PI/WIDTH;
-        this.boatY1 =0;
-        this.boatY =0;
-        this.boatY2 =0;
-        this.boatAngle =0;
+        this.FREQUENCY = 2 * Math.PI / WIDTH;
+        this.boatY1 = 0;
+        this.boatY = 0;
+        this.boatY2 = 0;
+        this.boatAngle = 0;
     }
 
+
+    // Draw the sine wave by calculating the points' positions on the canvas. f(x) = amplitude * sin(frequency * x + phase shift)
     sineWave.prototype.draw = function () {
         ctx.save();
         ctx.beginPath();
-        ctx.translate(0,WAVEPOSITION);
-        for (var x = 0; x < WIDTH; x +=5) {
+        ctx.translate(0, WAVEPOSITION);
+        for (var x = 0; x <= WIDTH + 5; x += 5) {
             var xpos = x,
-                ypos = Math.sin(this.VALUE * x + this.counter / 100) * this.amplitude;
+                ypos = Math.sin(this.FREQUENCY * x + this.counter / 100) * this.amplitude;
             ctx.lineTo(xpos, ypos);
-            if(x === 100){
-                this.boatY1= ypos;
-            }
-            else if(x === 125){
+
+            // Save the y-positions of the current frame to calculate the angle of rotation for the boat
+            if (x === 100) {
+                this.boatY1 = ypos;
+            } else if (x === 125) {
                 this.boatY = ypos;
+            } else if (x === 150) {
+                this.boatY2 = ypos;
             }
-            else if(x === 150)
-                this.boatY2 =ypos;
+
         }
+
         ctx.lineTo(WIDTH, HEIGHT);
         ctx.lineTo(0, HEIGHT);
         ctx.closePath();
@@ -80,15 +89,21 @@ var WaveDrawer = function () {
         ctx.restore();
 
         this.counter++;
-        this.boatAngle = -Math.atan((this.boatY1-this.boatY2)/50);
+
+        // Reset counter
+        if (this.counter >= 628) {
+            this.counter = 1;
+        }
+
+        this.boatAngle = -Math.atan((this.boatY1 - this.boatY2) / 50);
     };
 
     function drawBoat() {
         ctx.save();
-        ctx.translate(125,WAVEPOSITION+wave.boatY);
+        ctx.translate(125, WAVEPOSITION + wave.boatY);
         ctx.rotate(wave.boatAngle);
-       console.log(wave.boatAngle);
-        
+
+        // Draw the body of the boat
         ctx.beginPath();
         ctx.moveTo(-25, 0);
         ctx.quadraticCurveTo(-37, 0, -50, -20);
@@ -97,6 +112,7 @@ var WaveDrawer = function () {
         ctx.closePath();
         ctx.fill();
 
+        // Draw the larger triangle
         ctx.beginPath();
         ctx.moveTo(-37, -25);
         ctx.quadraticCurveTo(-13, -50, 0, -100);
@@ -104,33 +120,35 @@ var WaveDrawer = function () {
         ctx.closePath();
         ctx.fill();
 
+        // Draw the smaller triangle
         ctx.beginPath();
         ctx.moveTo(5, -25);
         ctx.lineTo(5, -75);
         ctx.quadraticCurveTo(10, -55, 37, -25);
         ctx.closePath();
         ctx.fill();
-        
+
         ctx.restore();
-        
-        
     }
 
-    function draw(timestamp) {
+
+    // Draw the graph for the current frame
+    function draw() {
         ctx.clearRect(0, 0, canvas.width, canvas.height);
         wave.draw();
         drawBoat();
         window.requestAnimationFrame(draw);
     }
 
+    // Call the function when the size of the canvas is changed
     function resize(width, height) {
         setCanvasSize(width, height);
         canvas.width = width;
         canvas.height = height;
         ctx.fillStyle = '#42A5F5';
-
     }
 
+    // Expose only the public functions to the user
     return {
         init: init,
         setCanvasID: setCanvasID,
