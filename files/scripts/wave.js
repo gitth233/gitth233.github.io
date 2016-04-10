e3967e8ba1d27cc3d9d5d6c720be0608fec5c3dd
Tan He <tan.he@uwaterloo.ca>
2016-04-09 23:00:04 -0400

    Added a boat to the canvas

--- files/scripts/wave.js
+++ files/scripts/wave.js
@@ -13,13 +13,13 @@ var WaveDrawer = function () {
         ctx = canvas.getContext('2d');
         canvas.width = WIDTH;
         canvas.height = HEIGHT;
-        ctx.fillStyle = '#BBDEFB';
+        ctx.fillStyle = '#42A5F5';
         ctx.lineJoin = 'round';
 
         wave = new sineWave();
-        if(WIDTH <=640)
-            wave.VALUE = Math.PI /100;
-        
+        if (WIDTH <= 640)
+            wave.VALUE = Math.PI / 100;
+
         window.requestAnimationFrame(draw);
     }
 
@@ -49,28 +49,77 @@ var WaveDrawer = function () {
         this.lineWidth = WIDTH / 100;
         this.amplitude = 30;
         this.counter = 1;
-        this.VALUE = 2 * Math.PI / 100;
+        this.VALUE = 2 * Math.PI/WIDTH;
+        this.boatY1 =0;
+        this.boatY =0;
+        this.boatY2 =0;
+        this.boatAngle =0;
     }
 
     sineWave.prototype.draw = function () {
+        ctx.save();
         ctx.beginPath();
-        ctx.moveTo(0, WAVEPOSITION);
-        for (var x = 0; x < WIDTH; x += 1) {
-            var xpos = x * this.lineWidth,
-                ypos = WAVEPOSITION + Math.sin(this.VALUE * x + this.counter / 100) * this.amplitude;
+        ctx.translate(0,WAVEPOSITION);
+        for (var x = 0; x < WIDTH; x +=5) {
+            var xpos = x,
+                ypos = Math.sin(this.VALUE * x + this.counter / 100) * this.amplitude;
             ctx.lineTo(xpos, ypos);
+            if(x === 100){
+                this.boatY1= ypos;
+            }
+            else if(x === 125){
+                this.boatY = ypos;
+            }
+            else if(x === 150)
+                this.boatY2 =ypos;
         }
         ctx.lineTo(WIDTH, HEIGHT);
         ctx.lineTo(0, HEIGHT);
         ctx.closePath();
         ctx.fill();
+        ctx.restore();
 
         this.counter++;
+        this.boatAngle = -Math.atan((this.boatY1-this.boatY2)/50);
     };
+
+    function drawBoat() {
+        ctx.save();
+        ctx.translate(125,WAVEPOSITION+wave.boatY);
+        ctx.rotate(wave.boatAngle);
+       console.log(wave.boatAngle);
+        
+        ctx.beginPath();
+        ctx.moveTo(-25, 0);
+        ctx.quadraticCurveTo(-37, 0, -50, -20);
+        ctx.lineTo(55, -20);
+        ctx.quadraticCurveTo(37, 0, 25, 0);
+        ctx.closePath();
+        ctx.fill();
+
+        ctx.beginPath();
+        ctx.moveTo(-37, -25);
+        ctx.quadraticCurveTo(-13, -50, 0, -100);
+        ctx.lineTo(0, -25);
+        ctx.closePath();
+        ctx.fill();
+
+        ctx.beginPath();
+        ctx.moveTo(5, -25);
+        ctx.lineTo(5, -75);
+        ctx.quadraticCurveTo(10, -55, 37, -25);
+        ctx.closePath();
+        ctx.fill();
+        
+        ctx.restore();
+        
+        
+    }
 
     function draw(timestamp) {
         ctx.clearRect(0, 0, canvas.width, canvas.height);
         wave.draw();
+        drawBoat();
         window.requestAnimationFrame(draw);
     }
 
@@ -78,7 +127,7 @@ var WaveDrawer = function () {
         setCanvasSize(width, height);
         canvas.width = width;
         canvas.height = height;
-        ctx.fillStyle = '#BBDEFB';
+        ctx.fillStyle = '#42A5F5';
 
     }
 
