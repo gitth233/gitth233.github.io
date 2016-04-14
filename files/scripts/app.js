7cc806cfa4020b7da2aaa8a776a7771a495f880b
Tan He <tan.he@uwaterloo.ca>
2016-04-13 21:29:40 -0400

    Revert "Refactored app.js and fixed bugs"
    This reverts commit 536836c5ae27014072423c21cf8437042d885d73.

--- files/scripts/app.js
+++ files/scripts/app.js
@@ -1,102 +1,88 @@
-$(document).ready(function () {
+(function () {
     var headerCanvas,
         footerCanvas,
         waveCanvas,
         canvasWidth = $('#flow-canvas').parent().innerWidth(),
         headerHeight = $('#flow-canvas').parent().innerHeight(),
-        footerHeight = $('#footer-canvas').parent().innerHeight(),
-        mobileCheck = ($('.navbar-toggle.collapsed').css('display') == 'none');
+        footerHeight = $('#footer-canvas').parent().innerHeight();
 
-    // Bootstrap popover
-    $('[data-toggle="popover"]').popover({
-        trigger: 'hover',
-        html: true,
-        template: '<div class="popover"><div class="arrow"></div><h3 class="popover-title"></h3><div class="popover-content"></div><p class ="align-right">- University of Waterloo</p><div class="popover-footer"></div></div>'
-    });
+    $(document).ready(function () {
 
-    // Bootstrap tooltip
-    $('[data-toggle="tooltip"]').tooltip({
-        trigger: 'hover'
-    });
-
-    // Scroll to the targeted location smoothly
-    $('a[href^="#"]').on('click', function (e) {
-        e.preventDefault();
-
-        var target = this.hash,
-            $target = $(target);
-
-        if (target == '#about' && !$('.dropdown-button').hasClass('active')) {
-            $('.dropdown-button').click();
-        }
-        $('html, body').stop().animate({
-            scrollTop: $target.offset().top
-        }, 900, 'swing', function () {
-            window.location.hash = target;
+        $('[data-toggle="popover"]').popover({
+            trigger: 'hover',
+            html: true,
+            template: '<div class="popover"><div class="arrow"></div><h3 class="popover-title"></h3><div class="popover-content"></div><p class ="align-right">- University of Waterloo</p><div class="popover-footer"></div></div>'
         });
-    });
 
-    // Change navbar background color and style
-    if (mobileCheck) {
-        // Bind an event handler to the window object
+        $('[data-toggle="tooltip"]').tooltip({
+            trigger: 'hover'
+        });
+
+        $('a[href^="#"]').on('click', function (e) {
+            e.preventDefault();
+
+            var target = this.hash,
+                $target = $(target);
+
+            if (target == '#about' && !$('.dropdown-button').hasClass('active')) {
+                $('.dropdown-button').click();
+            }
+            $('html, body').stop().animate({
+                scrollTop: $target.offset().top
+            }, 900, 'swing', function () {
+                window.location.hash = target;
+            });
+        });
+
+        var slide = function () {
+            $('.dropdown-button').toggleClass('active');
+            $('#about').slideToggle('700');
+        };
+
         $(window).scroll(function () {
-            if ($(window).scrollTop() > (headerHeight - 30)) {
+            if ($(window).scrollTop() > 506) {
                 $('.navbar-inverse').addClass('solid');
-            } else if ($(window).scrollTop() <= (headerHeight - 30)) {
+            } else if ($(window).scrollTop() <= 506) {
                 $('.navbar-inverse.solid').removeClass('solid');
             }
         });
-    } else {
-        // If the navbar collapse toggle is shown, give navbar a solid-color background and border 
-        $('.navbar-inverse').addClass('mobile').addClass('solid');
-    }
 
-    // Navigate to the top when nav-icon is clicked
-    $('.nav-icon').click(function () {
-        $('html, body').stop().animate({
-            scrollTop: '0px'
-        }, 900);
+        $('.nav-icon').click(function () {
+            $('html, body').stop().animate({
+                scrollTop: '0px'
+            }, 900);
+        });
+
+        $('.dropdown-button').click(slide);
+        $('.close-bar span').click(slide);
+
+        headerCanvas = new FlowDrawer();
+        headerCanvas.setCanvasID('flow-canvas');
+        headerCanvas.setCanvasSize(canvasWidth, headerHeight);
+        headerCanvas.setObjNum(20, 15, 5);
+        headerCanvas.init();
+
+        waveCanvas = new WaveDrawer();
+        waveCanvas.setCanvasID('wave-canvas');
+        waveCanvas.setCanvasSize(canvasWidth, headerHeight);
+        waveCanvas.setWavePosition(450);
+        waveCanvas.init();
+        
+        footerCanvas = new FlowDrawer();
+        footerCanvas.setCanvasID('footer-canvas');
+        footerCanvas.setCanvasSize(canvasWidth,footerHeight);
+        footerCanvas.setStrokeStyle('#616161');
+        footerCanvas.setObjNum(5,3,2);
+        footerCanvas.init();
     });
 
-    var slide = function () {
-        $('.dropdown-button').toggleClass('active');
-        $('#about').slideToggle('700');
-    };
 
-    // Bind an event handler to the two elements
-    $('.dropdown-button').click(slide);
-    $('.close-bar span').click(slide);
-
-
-    // Initialize header animation
-    headerCanvas = new FlowDrawer();
-    headerCanvas.setCanvasID('flow-canvas');
-    headerCanvas.setCanvasSize(canvasWidth, headerHeight);
-    headerCanvas.setObjNum(20, 15, 5);
-    headerCanvas.init();
-
-    // Initialize header animation
-    waveCanvas = new WaveDrawer();
-    waveCanvas.setCanvasID('wave-canvas');
-    waveCanvas.setCanvasSize(canvasWidth, headerHeight);
-    waveCanvas.setWavePosition(450);
-    waveCanvas.init();
-
-    // Initialize footer animation
-    footerCanvas = new FlowDrawer();
-    footerCanvas.setCanvasID('footer-canvas');
-    footerCanvas.setCanvasSize(canvasWidth, footerHeight);
-    footerCanvas.setStrokeStyle('#616161');
-    footerCanvas.setObjNum(5, 3, 2);
-    footerCanvas.init();
-
-    // Bind an event handler to the window
     $(window).resize(function () {
         canvasWidth = $('#flow-canvas').parent().innerWidth();
         headerHeight = $('#flow-canvas').parent().innerHeight();
         footerHeight = $('#footer-canvas').parent().innerHeight();
         headerCanvas.resize(canvasWidth, headerHeight);
         waveCanvas.resize(canvasWidth, headerHeight);
-        footerCanvas.resize(canvasWidth, footerHeight);
+        footerCanvas.resize(canvasWidth,footerHeight);
     });
-});
+}());
